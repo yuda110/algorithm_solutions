185c627672743ead48af7952bde8c3e6aa5dd3d5
yuda <aydha0110@gmail.com>
2016-04-21 23:58:23 +0900

    update e33.py

--- e33.py
+++ e33.py
@@ -1,10 +1,10 @@
-﻿#분자 numerator / 분모 denominator
+﻿from fractions import Fraction
 
 exception_list = [11, 22, 33, 44, 55, 66, 77, 88, 99]
 
-def make_fraction_list():
-    fraction_list = []
-    for nume in range(11, 98):
+def get_interesting_formula():
+    result_fraction = 1
+    for nume in range(11, 99):
         for deno in range(10, 98):
             if nume <= deno or nume in exception_list or deno in exception_list:
                 pass
@@ -14,7 +14,16 @@ def make_fraction_list():
                 for num in nume_list:
                     if num in deno_list and num != '0':
                         overlap_num = num
-                        fraction_list.append([nume, deno, overlap_num])
-    return fraction_list
+                        nume_list.remove(overlap_num)
+                        interesting_nume = int(nume_list[0])
+                        deno_list.remove(overlap_num)
+                        interesting_deno = int(deno_list[0])
+                        try:
+                            if deno/nume == interesting_deno/interesting_nume:
+                                result_fraction = result_fraction * interesting_deno/interesting_nume
+                        except ZeroDivisionError as e:
+                            pass
+    result_deno = Fraction(result_fraction).limit_denominator().denominator
+    return result_deno
 
-print(make_fraction_list())
+print(get_interesting_formula())
