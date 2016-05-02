7a6776f712a624c5d504c43fef14f3749a3ba6f1
yuda <aydha0110@gmail.com>
2016-05-02 22:28:00 +0900

    update e32

--- e32.py
+++ e32.py
@@ -1,11 +1,26 @@
-﻿#python3
+﻿#1 ~ 9 pandigital
+import time
 import re
+from itertools import permutations
 
-def is_pandigital(formula):
-    nums = list(re.sub('\D', '', formula))
-    return all(num in nums for num in ['1', '2', '3', '4', '5', '6', '7', '8', '9'])
+def get_permutation_list():
+    permu_list = permutations('123456789', 9)
+    return permu_list
 
-def get_number_of_cases():
-    
+def get_pandigital_formula(permutation_list):
+    result_set = set()
+    for permu in permutation_list:
+        first_num = int(permu[0])
+        second_num = int(permu[1]+permu[2]+permu[3]+permu[4])
+        third_num = int(permu[5]+permu[6]+permu[7]+permu[8])
+        if first_num*second_num == third_num:
+            result_set.add(third_num)
+        first_num = int(permu[0]+permu[1])
+        second_num = int(permu[2]+permu[3]+permu[4])
+        if first_num*second_num == third_num:
+            result_set.add(third_num)
+    return sum(result_set)
 
-#print(is_pandigital('39 × 186 = 7254'))
+start = time.time()
+print(get_pandigital_formula(get_permutation_list()))
+print(time.time() - start)
