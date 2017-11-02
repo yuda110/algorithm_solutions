963113041ea5737e924fd312778d0391c7d12313
yuda <aydha0110@gmail.com>
2017-11-02 12:00:48 +0900

    make Codility folder / add one file

---
+++ Codility/lesson1/BinaryGap.py
@@ -0,0 +1,12 @@
+
+def solution(n):
+    bin_str = bin(n).replace('0b', '')
+    bin_gap_list = list(filter(None, bin_str.split('1')))
+    if bin_str[-1] == '0':
+        bin_gap_list.pop()
+    if bin_gap_list:
+        max_bin_gap = len(max(bin_gap_list, key=len))
+    else:
+        max_bin_gap = 0
+    
+    return max_bin_gap
