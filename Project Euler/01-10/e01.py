4fa5cf6b9244626afed23479aefcc475e16baea1
Dahyun Yu <aydha0110@gmail.com>
2019-02-01 11:05:24 +0900

    Update e01.py

--- Project Euler/01-10/e01.py
+++ Project Euler/01-10/e01.py
@@ -9,15 +9,23 @@
 #
 # print(result)
 
-def get_multiple(n_list):
-    sum = 0
-    for i in range(1, 1000):
-        remainder_list = [i%n for n in n_list]
-        if 0 in remainder_list:
-            sum += i
-    return sum
+# def get_multiple(n_list):
+#     sum = 0
+#     for i in range(1, 1000):
+#         remainder_list = [i%n for n in n_list]
+#         if 0 in remainder_list:
+#             sum += i
+#     return sum
 
-if __name__ == '__main__':
-    n_list = [3, 5]
-    sum = get_multiple(n_list)
-    print(sum)
+# if __name__ == '__main__':
+#     n_list = [3, 5]
+#     sum = get_multiple(n_list)
+#     print(sum)
+
+
+n = 1000
+a = ((n-1)//3)
+b = ((n-1)//5)
+c = ((n-1)//15)
+sum = (((3 * a * (a+1))//2) + ((5 * b * (b+1))//2) - ((15 * c * (c+1))//2))
+print(int(sum))
