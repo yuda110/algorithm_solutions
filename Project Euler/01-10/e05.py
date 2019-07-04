d8e80e050ffd1d8ee78c0fb3bd148208998264b0
Dahyun Yu <aydha0110@gmail.com>
2019-07-04 09:43:18 +0900

    Update e05

--- Project Euler/01-10/e05.py
+++ Project Euler/01-10/e05.py
@@ -1,24 +1,67 @@
-#python3
-def is_devidable(num_todo, num_to_devide):
-    return num_todo % num_to_devide == 0
+"""
+최소공배수 LCM(Lease Common Multiple)
 
-def is_devidable_at_range(num_todo,range_from, range_to ):
-    result = True
-    for i in range(range_from, range_to):
-        if not is_devidable(num_todo, i):
-            result = False
-            break
-    return result
+1 ~ 10 사이의 어떤 수로도 나누어 떨어지는 가장 작은 수는 2520입니다.
+그러면 1 ~ 20 사이의 어떤 수로도 나누어 떨어지는 가장 작은 수는 얼마입니까?
+"""
 
-def find_devidable_min_value(range_from, range_to) :
-    num_todo = range_to
-    while True :
-        if num_todo % range_to == 0 :
-            if not is_devidable_at_range(num_todo, range_from, range_to) :
-                print(num_todo)
-            else :
+import functools
+import operator
+
+from py_modules.prime import prime_list
+from py_decorators.timer import logging_time
+
+
+def get_prime_set(num):
+    prime_set = set()
+    for i in range(2, num):
+        primes = prime_list(i)
+        prime_set.update(primes) if primes else prime_set.add(i)
+    return prime_set
+
+
+@logging_time
+def get_lcm():
+    prime_set = get_prime_set(20)
+    step = functools.reduce(operator.mul, prime_set)    # 소수들을 곱한 수
+    result = step
+    while True:
+        for i in range(1, 21):
+            if result % i != 0:
                 break
-        num_todo += range_to
-    return num_todo
+            elif i == 20:
+                return result
+        result += step
 
-print(find_devidable_min_value(1, 20))
+
+if __name__ == '__main__':
+    print(get_lcm())
+
+
+# def is_devidable(num_todo, num_to_devide):
+#     return num_todo % num_to_devide == 0
+#
+#
+# def is_devidable_at_range(num_todo,range_from, range_to ):
+#     result = True
+#     for i in range(range_from, range_to):
+#         if not is_devidable(num_todo, i):
+#             result = False
+#             break
+#     return result
+
+
+# def find_devidable_min_value(range_from, range_to) :
+#     num_todo = range_to
+#     while True :
+#         if num_todo % range_to == 0 :
+#             if not is_devidable_at_range(num_todo, range_from, range_to) :
+#                 pass
+#                 # print(num_todo)
+#             else :
+#                 break
+#         num_todo += range_to
+#     return num_todo
+#
+#
+# print(find_devidable_min_value(1, 20))
