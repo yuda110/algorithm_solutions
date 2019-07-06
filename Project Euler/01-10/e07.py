a86f0850638bbbf9de39c7ce6344ed3d3e69c114
Dahyun Yu <aydha0110@gmail.com>
2019-07-06 09:11:40 +0900

    Update e07

--- Project Euler/01-10/e07.py
+++ Project Euler/01-10/e07.py
@@ -1,5 +1,11 @@
-#python3
-import time
+"""
+소수를 크기 순으로 나열하면 2, 3, 5, 7, 11, 13, ... 과 같이 됩니다.
+이 때 10,001번째의 소수를 구하세요.
+"""
+
+from py_modules.prime import is_prime
+from py_modules.timer import logging_time
+
 
 #def is_prime_num(num) :    #16.15 seconds
 #    i = 3
@@ -9,23 +15,27 @@ import time
 #        else :
 #            return False
 #    return True
-def is_prime_num(num) :     #0.21 seconds
-    for i in range(2, int(num**0.5) + 1):
-        if num % i==0:
-            return False
-    return True
 
-def find_prime_num(seq_range_to) :
+
+# def is_prime_num(num):   # 0.21 seconds
+#     for i in range(2, int(num**0.5) + 1):
+#         if num % i == 0:
+#             return False
+#     return True
+
+
+@logging_time
+def find_prime_num(seq_range_to):
     num = 3
     prime_seq = 1
     prime_num = 0
-    while prime_seq < seq_range_to :
-        if is_prime_num(num) == True :
+    while prime_seq < seq_range_to:
+        if is_prime(num):
             prime_seq += 1
             prime_num = num
         num += 2
     return prime_num
 
-start_time = time.time()
-print(find_prime_num(10001))
-print("--- %s seconds ---" % (time.time() - start_time))
+
+if __name__ == '__main__':
+    print(find_prime_num(10001))
--- py_modules/prime.py
+++ py_modules/prime.py
@@ -1,9 +1,10 @@
 # Prime module
 
 
+# 단, n은 2 이상인 수
 def is_prime(n):
     for i in range(2, int(n**0.5)+1):
-        if n % i == 0 or n == 1:
+        if n % i == 0:
             return False
     return True
 
