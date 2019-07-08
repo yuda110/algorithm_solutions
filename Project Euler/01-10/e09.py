14aa4dfa543ebaeafd59a95aa83ee6753d373c17
Dahyun Yu <aydha0110@gmail.com>
2019-07-08 11:09:36 +0900

    Update e09

--- Project Euler/01-10/e09.py
+++ Project Euler/01-10/e09.py
@@ -1,30 +1,21 @@
-#python3
-import time
+"""
+세 자연수 a, b, c 가 피타고라스 정리 a2 + b2 = c2 를 만족하면 피타고라스 수라고 부릅니다 (여기서 a < b < c ).
+예를 들면 32 + 42 = 9 + 16 = 25 = 52이므로 3, 4, 5는 피타고라스 수입니다.
 
-def is_pita(a, b, c) :
-    if(a*a + b*b == c*c) :
-        return True
-    else :
-        return False
+a + b + c = 1000 인 피타고라스 수 a, b, c는 한 가지 뿐입니다. 이 때, a × b × c 는 얼마입니까?
+"""
 
-#21seconds
-#def find_pita() :
-#    for c in range(3, 1000) :
-#        for b in range(2, 1000) :
-#            for a in range(1, 1000) :
-#                if  a<b and b<c and is_pita(a, b, c) :
-#                    if a+b+c == 1000 :
-#                        return a*b*c
+from py_modules.timer import logging_time
 
-# 7.97seconds > without c for loop 0.08
-def find_pita() :
-    for a in range(1, 1000) :
-        for b in range(a+1, 1000) :
+# 0.03sec
+@logging_time
+def find_pita():
+    for a in range(1, 1000):
+        for b in range(a+1, 1000):
             c = 1000 - a - b
-            if a+b+c == 1000  :
-                if  is_pita(a, b, c):
-                    return a*b*c
+            if a*a + b*b == c*c and a < b < c:
+                return a*b*c
 
-start_time = time.time()
-print(find_pita())
-print("--- %s seconds ---" % (time.time() - start_time))
+
+if __name__ == '__main__':
+    find_pita()
