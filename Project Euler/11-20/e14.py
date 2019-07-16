c57cd8d8dc3e88d4aa8108bb19a593ca1cf3c798
Dahyun Yu <aydha0110@gmail.com>
2019-07-16 09:49:08 +0900

    Update e14

--- Project Euler/11-20/e14.py
+++ Project Euler/11-20/e14.py
@@ -1,27 +1,29 @@
-#python3
-import time
+"""
+백만 이하로 시작하는 우박수(hailstone sequence) 중 가장 긴 과정을 거치는 것은?
+"""
+from py_modules.timer import logging_time
 
-def cal_hailstone_sequence(num) :
-    seq = 0
-    while num > 1 :
-        if num % 2 == 0 :
-            num = num / 2
-        else :
-            num = 3*num + 1
-        seq += 1
-    return seq+1
 
-def who_has_max_seq(range_to) :
-    i = 1
+def cal_hailstone_sequence(num):
+    cnt = 1
+    while num > 1:
+        num = num // 2 if num % 2 == 0 else 3 * num + 1
+        cnt += 1
+    return cnt
+
+
+@logging_time
+def who_has_max_seq(num):
     hailstone = 0
     max_seq = 0
-    while i < range_to :
-        if cal_hailstone_sequence(i) > max_seq :
-            max_seq = cal_hailstone_sequence(i)
+    for i in range(num // 2, num+1):
+        seq = cal_hailstone_sequence(i)
+        if seq > max_seq:
+            max_seq = seq
             hailstone = i
-        i += 2
+    print(hailstone)
     return hailstone
 
-start_time = time.time()
-#print(who_has_max_seq(1000000))    #24seconds
-print("--- %s seconds ---" % (time.time() - start_time)) 
+
+if __name__ == '__main__':
+    who_has_max_seq(1000000)        # 8.25 sec
