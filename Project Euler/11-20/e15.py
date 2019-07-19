4ffa2c8f6b4c0b142b3fc1d64f0920a94250d6b9
Dahyun Yu <aydha0110@gmail.com>
2019-07-19 09:13:51 +0900

    Update e15

--- Project Euler/11-20/e15.py
+++ Project Euler/11-20/e15.py
@@ -1,11 +1,20 @@
-#python3
-def cal_number_of_case(range_to) :
-    pre_case = [1]*range_to
-    next_case = [1]*range_to
-    while pre_case[1] < range_to :
-        for i in range(1, range_to) : 
+"""
+20×20 격자의 좌상단에서 우하단으로 가는 경로의 수
+중학교 수학 문제
+"""
+from py_modules.timer import logging_time
+
+
+@logging_time
+def cal_number_of_case():
+    pre_case = [1]*21
+    next_case = [1]*21
+    while pre_case[1] < 21:
+        for i in range(1, 21):
             next_case[i] = next_case[i-1] + pre_case[i]
             pre_case = next_case
     return next_case[-1]
 
-print(cal_number_of_case(21))
+
+if __name__ == '__main__':
+    print(cal_number_of_case())
