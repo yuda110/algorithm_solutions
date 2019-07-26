a667da984c674a9ac41b4b7b156cc8123ed4cc5e
Dahyun Yu <aydha0110@gmail.com>
2019-07-26 09:33:29 +0900

    Update e20

--- Project Euler/11-20/e20.py
+++ Project Euler/11-20/e20.py
@@ -1,11 +1,27 @@
-#python3
+"""
+100! 의 자리수를 모두 더하면 얼마입니까?
+"""
+from py_modules.timer import logging_time
 import math
 
-def sum_factorial(num) :
+
+@logging_time
+def solution1(num):
     facto = math.factorial(num)
     facto_arr = list(str(facto))
     facto_sum = sum(int(i) for i in facto_arr)
-
     return facto_sum
 
-print(sum_factorial(100))
+
+@logging_time
+def solution2(num):
+    f = 1
+    for i in range(1, num+1):
+        f *= i
+    result = sum([int(i) for i in str(f)])
+    return result
+
+
+if __name__ == '__main__':
+    solution1(100)
+    solution2(100)
