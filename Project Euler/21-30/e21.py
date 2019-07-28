d75f3b9c8eebb8fb8200268ba0460ec2f01c27e0
Dahyun Yu <aydha0110@gmail.com>
2019-07-28 13:50:34 +0900

    Update e21

--- Project Euler/21-30/e21.py
+++ Project Euler/21-30/e21.py
@@ -1,26 +1,29 @@
-#python3
-import time
+"""
+서로 다른 두 정수 a, b에 대하여 d(a) = b 이고 d(b) = a 이면
+a, b는 친화쌍이라 하고 a와 b를 각각 친화수(우애수)라고 합니다.
+10000 이하의 친화수들을 모두 찾아서 그 합을 구하세요.
+"""
+from py_modules.timer import logging_time
 
-# splitter.py
-def d(num) :
-    factors = set([1, num])
-    for i in range(2, num) :
-        if i in factors :
-            return int(sum(factors)-num)
-        elif num % i == 0 :
+
+def d(num):
+    factors = {1}
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0:
             factors.add(i)
-            factors.add(num/i)
-    return int(sum(factors)-num)
+            factors.add(num//i)
+    return sum(factors)
 
-def sum_amicable_numbers(range_to) :
-    sum_ami = 0
 
-    for a in range(2, range_to) :
+@logging_time
+def solution(range_to):
+    result = 0
+    for a in range(2, range_to):
         b = d(a)
-        if a == d(b) and a != b :
-            sum_ami += a
-    return sum_ami
+        if a == d(b) and a != b:
+            result += a
+    return result
 
-start_time = time.time()
-print(sum_amicable_numbers(10000))
-print("--- %s seconds ---" % (time.time() - start_time))
+
+if __name__ == '__main__':
+    solution(10000)
