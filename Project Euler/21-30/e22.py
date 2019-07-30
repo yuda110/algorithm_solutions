b52844e6f3f8d756b01408e94030e18e22f1338c
Dahyun Yu <aydha0110@gmail.com>
2019-07-30 09:17:36 +0900

    Update e22

--- Project Euler/21-30/e22.py
+++ Project Euler/21-30/e22.py
@@ -1,36 +1,33 @@
-#python3
-import os
-import re
+"""
+먼저 모든 이름을 알파벳 순으로 정렬합니다.
+각 이름에 대해서, 그 이름을 이루는 알파벳에 해당하는 숫자(A=1, B=2, ..., Z=26)를 모두 더합니다.
+여기에 이 이름의 순번을 곱합니다.
 
-def alphabetize() :
-    f = open("C:\\names.txt")
-    names = f.readline()
-    name_arr = re.findall('\w+', names)
-    name_arr.sort()
-    return name_arr
+names.txt에 들어있는 모든 이름의 점수를 계산해서 더하면 얼마입니까?
+"""
+from py_modules.timer import logging_time
 
-def chg_alphabet_to_num() :
-    alphabets = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-    alpha_dict = {}
-    seq = 1
-    for i in alphabets :
-        alpha_dict[i] = seq
-        seq += 1
-    return alpha_dict
 
-def sum_and_multiple_alphabet_in_name() :
-    alpha_dict = chg_alphabet_to_num() 
-    name_arr = alphabetize()
-    sum_multi_sum = 0
-    seq = 1
-    for name in name_arr :
-        sum = 0
-        sum_multi = 0
-        for i in name :
-            sum += alpha_dict[i]
-        sum_multi = sum * (seq)
-        sum_multi_sum += sum_multi
-        seq += 1
-    return sum_multi_sum
+def get_name_list():
+    f = open('names.txt', 'r')
+    names = f.read()
+    name_list = names.replace('"', "").split(',')
+    name_list.sort()
+    return name_list
 
-print(sum_and_multiple_alphabet_in_name())
+
+@logging_time
+def solution():
+    a_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+              'U', 'V', 'W', 'X', 'Y', 'Z']
+    name_list = get_name_list()
+    result = 0
+    for idx, name in enumerate(name_list, 1):
+        score = sum([a_list.index(a)+1 for a in name])
+        score *= idx
+        result += score
+    return result
+
+
+if __name__ == '__main__':
+    solution()
